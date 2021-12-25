9a70ba3bd9adee759caa6b6cf711d99558ccf118
VanderLord008 <90377070+VanderLord008@users.noreply.github.com>
2021-12-25 12:24:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,199 @@
+window.addEventListener("keydown",function(evt)
+{
+    if(evt.key==="a")
+    {
+        let key1=this.document.querySelector(".keyA");
+        let aud1=this.document.querySelector("#audA");
+        key1.classList.add("funA");
+        aud1.play();
+    }
+})
+
+window.addEventListener("keyup",function(evt)
+{
+    if(evt.key==="a")
+    {
+        let key1=this.document.querySelector(".keyA");
+        
+        key1.classList.remove("funA");
+        
+    }
+})
+
+
+
+window.addEventListener("keydown",function(evt)
+{
+    if(evt.key==="s")
+    {
+        let key2=this.document.querySelector(".keyS");
+        let aud2=this.document.querySelector("#audS");
+        key2.classList.add("funS");
+        aud2.play();
+    }
+})
+
+window.addEventListener("keyup",function(evt)
+{
+    if(evt.key==="s")
+    {
+        let key2=this.document.querySelector(".keyS");
+        
+        key2.classList.remove("funS");
+        
+    }
+})
+
+window.addEventListener("keyup",function(evt)
+{
+    if(evt.key==="d")
+    {
+        let key2=this.document.querySelector(".keyD");
+        
+        key2.classList.remove("funD");
+        
+    }
+})
+
+window.addEventListener("keydown",function(evt)
+{
+    if(evt.key==="d")
+    {
+        let key2=this.document.querySelector(".keyD");
+        let aud2=this.document.querySelector("#audD");
+        key2.classList.add("funD");
+        aud2.play();
+    }
+})
+
+window.addEventListener("keyup",function(evt)
+{
+    if(evt.key==="f")
+    {
+        let key2=this.document.querySelector(".keyF");
+        
+        key2.classList.remove("funF");
+        
+    }
+})
+
+window.addEventListener("keydown",function(evt)
+{
+    if(evt.key==="f")
+    {
+        let key2=this.document.querySelector(".keyF");
+        let aud2=this.document.querySelector("#audF");
+        key2.classList.add("funF");
+        aud2.play();
+    }
+})
+
+window.addEventListener("keyup",function(evt)
+{
+    if(evt.key==="g")
+    {
+        let key2=this.document.querySelector(".keyG");
+        
+        key2.classList.remove("funG");
+        
+    }
+})
+
+window.addEventListener("keydown",function(evt)
+{
+    if(evt.key==="g")
+    {
+        let key2=this.document.querySelector(".keyG");
+        let aud2=this.document.querySelector("#audG");
+        key2.classList.add("funG");
+        aud2.play();
+    }
+})
+
+window.addEventListener("keyup",function(evt)
+{
+    if(evt.key==="h")
+    {
+        let key2=this.document.querySelector(".keyH");
+        
+        key2.classList.remove("funH");
+        
+    }
+})
+
+window.addEventListener("keydown",function(evt)
+{
+    if(evt.key==="h")
+    {
+        let key2=this.document.querySelector(".keyH");
+        let aud2=this.document.querySelector("#audH");
+        key2.classList.add("funH");
+        aud2.play();
+    }
+})
+
+window.addEventListener("keyup",function(evt)
+{
+    if(evt.key==="j")
+    {
+        let key2=this.document.querySelector(".keyJ");
+        
+        key2.classList.remove("funJ");
+        
+    }
+})
+
+window.addEventListener("keydown",function(evt)
+{
+    if(evt.key==="j")
+    {
+        let key2=this.document.querySelector(".keyJ");
+        let aud2=this.document.querySelector("#audJ");
+        key2.classList.add("funJ");
+        aud2.play();
+    }
+})
+
+window.addEventListener("keyup",function(evt)
+{
+    if(evt.key==="k")
+    {
+        let key2=this.document.querySelector(".keyK");
+        
+        key2.classList.remove("funK");
+        
+    }
+})
+
+window.addEventListener("keydown",function(evt)
+{
+    if(evt.key==="k")
+    {
+        let key2=this.document.querySelector(".keyK");
+        let aud2=this.document.querySelector("#audK");
+        key2.classList.add("funK");
+        aud2.play();
+    }
+})
+
+window.addEventListener("keyup",function(evt)
+{
+    if(evt.key==="l")
+    {
+        let key2=this.document.querySelector(".keyL");
+        
+        key2.classList.remove("funL");
+        
+    }
+})
+
+window.addEventListener("keydown",function(evt)
+{
+    if(evt.key==="l")
+    {
+        let key2=this.document.querySelector(".keyL");
+        let aud2=this.document.querySelector("#audL");
+        key2.classList.add("funL");
+        aud2.play();
+    }
+})
